c7a0dee633317157754d8cadefbc167536562adc
Developer-Rathod <irina.bedunkevits@gmail.com>
2021-12-02 14:13:06 +0200

    Irinas commitments

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import homeData from '../../assets/mockdata/home.json';
+import { DrawerService } from '../services/drawer.service';
+ 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+ 
+  sections = homeData.sections;
+  spotlight = homeData.spotlight;
+ 
+  opts = {
+    slidesPerView: 2.4,
+    spaceBetween: 10,
+    freeMode: true
+  };
+ 
+  constructor(private modalCtrl: ModalController, private drawerService: DrawerService) {
+  }
+ 
+  openInfo(series) {
+    
+  }
+ 
+  async openCategories() {
+ 
+  }
+ 
 }
